f6cf79706b4a1da95b0c52bdef3ec342a726b562
Bence Kiglics <bence.kiglics@gmail.com>
2016-10-29 17:24:03 +0200

    Added test for !=

--- filter/columnfilter_test.go
+++ filter/columnfilter_test.go
@@ -26,3 +26,17 @@ func TestShouldNotPanicIfColumnIndexIsWrong(t *testing.T) {
 
 	filter.Match(testLine)
 }
+
+func TestSimpleNotEqualsFilterWorks(t *testing.T) {
+	filter := NewColumnFilter("$3 != \"teststring\"")
+	testLine1 := &parser.LogLine{Columns: []string{"", "", "teststring"}}
+	testLine2 := &parser.LogLine{Columns: []string{"", "", "string"}}
+
+	if filter.Match(testLine1) {
+		t.Errorf("testLine1 (%s) should match filter (%s)", testLine1, filter)
+	}
+
+	if !filter.Match(testLine2) {
+		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine1, filter)
+	}
+}
