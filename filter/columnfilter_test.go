d53b99b1942fa1bbcbf001d8591fb7fe8a4d2681
Bence Kiglics <bence.kiglics@gmail.com>
2016-10-31 07:46:59 +0100

    Refactored tests to make them more readable

--- filter/columnfilter_test.go
+++ filter/columnfilter_test.go
@@ -6,114 +6,78 @@ import (
 	"github.com/kbence/logan/parser"
 )
 
+type expectation struct {
+	Matches bool
+	Line    *parser.LogLine
+}
+
+func newExpectation(matches bool, columns ...string) *expectation {
+	return &expectation{Matches: matches, Line: &parser.LogLine{Columns: columns}}
+}
+
+func expectMatch(columns ...string) *expectation {
+	return newExpectation(true, columns...)
+}
+
+func expectDoesntMatch(columns ...string) *expectation {
+	return newExpectation(false, columns...)
+}
+
+func testFilter(t *testing.T, filterString string, expectations ...*expectation) {
+	filter := NewColumnFilter(filterString)
+
+	for _, exp := range expectations {
+		if exp.Matches {
+			if !filter.Match(exp.Line) {
+				t.Errorf("%s should%s match line", filter, exp.Line)
+			}
+		} else {
+			if filter.Match(exp.Line) {
+				t.Errorf("%s should%s match line", filter, exp.Line)
+			}
+		}
+	}
+}
+
 func TestSimpleEqualFilterWorks(t *testing.T) {
-	filter := NewColumnFilter("$3 == \"teststring\"")
-	testLine1 := &parser.LogLine{Columns: []string{"", "", "teststring"}}
-	testLine2 := &parser.LogLine{Columns: []string{"", "", "string"}}
+	testFilter(t, "$3 == \"teststring\"",
+		expectMatch("", "", "teststring"),
+		expectDoesntMatch("", "", "string"))
 
-	if !filter.Match(testLine1) {
-		t.Errorf("testLine1 (%s) should match filter (%s)", testLine1, filter)
-	}
-
-	if filter.Match(testLine2) {
-		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine2, filter)
-	}
 }
 
 func TestShouldNotPanicIfColumnIndexIsWrong(t *testing.T) {
-	filter := NewColumnFilter("$4 == \"teststring\"")
-	testLine := &parser.LogLine{Columns: []string{"", "", "teststring"}}
-
-	filter.Match(testLine)
+	testFilter(t, "$4 == \"teststring\"",
+		expectDoesntMatch("", "", "teststring"))
 }
 
 func TestSimpleNotEqualsFilterWorks(t *testing.T) {
-	filter := NewColumnFilter("$3 != \"teststring\"")
-	testLine1 := &parser.LogLine{Columns: []string{"", "", "teststring"}}
-	testLine2 := &parser.LogLine{Columns: []string{"", "", "string"}}
-
-	if filter.Match(testLine1) {
-		t.Errorf("testLine1 (%s) should match filter (%s)", testLine1, filter)
-	}
-
-	if !filter.Match(testLine2) {
-		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine2, filter)
-	}
+	testFilter(t, "$3 != \"teststring\"",
+		expectDoesntMatch("", "", "teststring"),
+		expectMatch("", "", "string"))
 }
 
 func TestAndWorksBetweenFilterExpressions(t *testing.T) {
-	filter := NewColumnFilter("$1 == \"1\" AND $3 == \"teststring\"")
-	testLineBothMatches := &parser.LogLine{Columns: []string{"1", "", "teststring"}}
-	testLineFirstMatches := &parser.LogLine{Columns: []string{"1", "", "string"}}
-	testLineSecondMatches := &parser.LogLine{Columns: []string{"2", "", "teststring"}}
-	testLineNothingMatches := &parser.LogLine{Columns: []string{"2", "", "string"}}
-
-	if !filter.Match(testLineBothMatches) {
-		t.Errorf("testLineBothMatches (%s) should match filter (%s)", testLineBothMatches, filter)
-	}
-
-	if filter.Match(testLineFirstMatches) {
-		t.Errorf("testLineFirstMatches (%s) should not match filter (%s)", testLineFirstMatches, filter)
-	}
-
-	if filter.Match(testLineSecondMatches) {
-		t.Errorf("testLineSecondMatches (%s) should not match filter (%s)", testLineSecondMatches, filter)
-	}
-
-	if filter.Match(testLineNothingMatches) {
-		t.Errorf("testLineNothingMatches (%s) should not match filter (%s)", testLineNothingMatches, filter)
-	}
+	testFilter(t, "$1 == \"1\" AND $3 == \"teststring\"",
+		expectMatch("1", "", "teststring"),
+		expectDoesntMatch("1", "", "string"),
+		expectDoesntMatch("2", "", "teststring"),
+		expectDoesntMatch("2", "", "string"))
 }
 
 func TestOrWorksBetweenFilterExpressions(t *testing.T) {
-	filter := NewColumnFilter("$1 == \"1\" OR $3 == \"teststring\"")
-	testLineBothMatches := &parser.LogLine{Columns: []string{"1", "", "teststring"}}
-	testLineFirstMatches := &parser.LogLine{Columns: []string{"1", "", "string"}}
-	testLineSecondMatches := &parser.LogLine{Columns: []string{"2", "", "teststring"}}
-	testLineNothingMatches := &parser.LogLine{Columns: []string{"2", "", "string"}}
-
-	if !filter.Match(testLineBothMatches) {
-		t.Errorf("testLineBothMatches (%s) should match filter (%s)", testLineBothMatches, filter)
-	}
-
-	if !filter.Match(testLineFirstMatches) {
-		t.Errorf("testLineFirstMatches (%s) should not match filter (%s)", testLineFirstMatches, filter)
-	}
-
-	if !filter.Match(testLineSecondMatches) {
-		t.Errorf("testLineSecondMatches (%s) should not match filter (%s)", testLineSecondMatches, filter)
-	}
-
-	if filter.Match(testLineNothingMatches) {
-		t.Errorf("testLineNothingMatches (%s) should not match filter (%s)", testLineNothingMatches, filter)
-	}
+	testFilter(t, "$1 == \"1\" OR $3 == \"teststring\"",
+		expectMatch("1", "", "teststring"),
+		expectMatch("1", "", "string"),
+		expectMatch("2", "", "teststring"),
+		expectDoesntMatch("2", "", "string"))
 }
 
 func TestOperatorPrecedence(t *testing.T) {
-	filter := NewColumnFilter("$1 == \"1\" OR $2 == \"second\" AND $3 == \"teststring\"")
-	testLineMatches1 := &parser.LogLine{Columns: []string{"1", "no match", "no match"}}
-	testLineMatches2 := &parser.LogLine{Columns: []string{"no match", "second", "teststring"}}
-	testLineMatches3 := &parser.LogLine{Columns: []string{"1", "second", "teststring"}}
-	testLineNoMatch1 := &parser.LogLine{Columns: []string{"no match", "second", "no match"}}
-	testLineNoMatch2 := &parser.LogLine{Columns: []string{"no match", "no match", "second"}}
-
-	if !filter.Match(testLineMatches1) {
-		t.Errorf("testLineMatches1 (%s) should match filter (%s)", testLineMatches1, filter)
-	}
-
-	if !filter.Match(testLineMatches2) {
-		t.Errorf("testLineMatches2 (%s) should match filter (%s)", testLineMatches2, filter)
-	}
-
-	if !filter.Match(testLineMatches3) {
-		t.Errorf("testLineMatches3 (%s) should match filter (%s)", testLineMatches3, filter)
-	}
-
-	if filter.Match(testLineNoMatch1) {
-		t.Errorf("testLineNoMatch1 (%s) should not match filter (%s)", testLineNoMatch1, filter)
-	}
-
-	if filter.Match(testLineNoMatch2) {
-		t.Errorf("testLineNoMatch2 (%s) should not match filter (%s)", testLineNoMatch2, filter)
-	}
+	testFilter(t, "$1 == \"1\" OR $2 == \"second\" AND $3 == \"teststring\"",
+		expectMatch("1", "no match", "no match"),
+		expectMatch("no match", "second", "teststring"),
+		expectMatch("1", "second", "teststring"),
+		expectDoesntMatch("no match", "second", "no match"),
+		expectDoesntMatch("no match", "no match", "second"))
 }
