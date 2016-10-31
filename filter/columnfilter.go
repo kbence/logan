f7abdd837d3c384f3ed60ecab11a7decd5119a8f
Bence Kiglics <bence.kiglics@gmail.com>
2016-10-31 07:39:39 +0100

    Implemented AND/OR operators between expressions

--- filter/columnfilter.go
+++ filter/columnfilter.go
@@ -1,6 +1,8 @@
 package filter
 
 import (
+	"bytes"
+	"fmt"
 	"strconv"
 
 	"github.com/kbence/logan/parser"
@@ -11,7 +13,11 @@ type Operator uint8
 const (
 	OpEquals Operator = iota
 	OpNotEquals
+	OpOr
+	OpAnd
 )
+
+var operatorStrings = []string{"==", "!=", "OR", "AND"}
 
 type ExpressionType uint8
 
@@ -19,6 +25,7 @@ const (
 	TypeLiteral ExpressionType = iota
 	TypeColumn
 	TypeRelation
+	TypeLogical
 )
 
 type Expression struct {
@@ -30,6 +37,37 @@ type Expression struct {
 	Literal string
 }
 
+func (e *Expression) String() string {
+	buf := bytes.NewBufferString("[Expression ")
+
+	if e == nil {
+		return "nil"
+	}
+
+	switch e.Type {
+	case TypeLiteral:
+		buf.WriteString(fmt.Sprintf("\"%s\"", e.Literal))
+		break
+
+	case TypeColumn:
+		buf.WriteString(fmt.Sprintf("$%s", e.Literal))
+		break
+
+	case TypeRelation:
+		buf.WriteString(fmt.Sprintf("%s %s %s", e.Left.String(),
+			operatorStrings[int(e.Op)], e.Right.String()))
+		break
+
+	case TypeLogical:
+		buf.WriteString(fmt.Sprintf("%s %s %s", e.Left.String(),
+			operatorStrings[int(e.Op)], e.Right.String()))
+		break
+	}
+
+	buf.WriteString("]")
+	return buf.String()
+}
+
 func (e *Expression) GoLeft() *Expression {
 	e.Left = &Expression{Parent: e}
 	return e.Left
@@ -38,6 +76,21 @@ func (e *Expression) GoLeft() *Expression {
 func (e *Expression) GoRight() *Expression {
 	e.Right = &Expression{Parent: e}
 	return e.Right
+}
+
+func (e *Expression) GoUp() *Expression {
+	return e.Parent
+}
+
+func (e *Expression) PushLeftGoRight(op Operator) *Expression {
+	newExpr := &Expression{Type: TypeLogical, Parent: e.Parent, Left: e, Op: op}
+	newExpr.Right = &Expression{Parent: newExpr}
+
+	if e.Parent != nil {
+		e.Parent.Right = newExpr
+	}
+
+	return newExpr.Right
 }
 
 func (e *Expression) SetType(t ExpressionType) {
@@ -71,12 +124,25 @@ func (e *Expression) EvaluateString(line *parser.LogLine) string {
 }
 
 func (e *Expression) EvaluateBool(line *parser.LogLine) bool {
-	switch e.Op {
-	case OpEquals:
-		return e.Left.EvaluateString(line) == e.Right.EvaluateString(line)
+	switch e.Type {
+	case TypeRelation:
+		switch e.Op {
+		case OpEquals:
+			return e.Left.EvaluateString(line) == e.Right.EvaluateString(line)
 
-	case OpNotEquals:
-		return e.Left.EvaluateString(line) != e.Right.EvaluateString(line)
+		case OpNotEquals:
+			return e.Left.EvaluateString(line) != e.Right.EvaluateString(line)
+		}
+		break
+
+	case TypeLogical:
+		switch e.Op {
+		case OpAnd:
+			return e.Left.EvaluateBool(line) && e.Right.EvaluateBool(line)
+
+		case OpOr:
+			return e.Left.EvaluateBool(line) || e.Right.EvaluateBool(line)
+		}
 	}
 
 	return false
@@ -93,6 +159,10 @@ func NewColumnFilter(filterExpression string) *ColumnFilter {
 	parser.Execute()
 
 	return &ColumnFilter{expr: parser.Expr}
+}
+
+func (f *ColumnFilter) String() string {
+	return f.expr.String()
 }
 
 func (f *ColumnFilter) Match(line *parser.LogLine) bool {
--- filter/columnfilter_test.go
+++ filter/columnfilter_test.go
@@ -16,7 +16,7 @@ func TestSimpleEqualFilterWorks(t *testing.T) {
 	}
 
 	if filter.Match(testLine2) {
-		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine1, filter)
+		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine2, filter)
 	}
 }
 
@@ -37,6 +37,83 @@ func TestSimpleNotEqualsFilterWorks(t *testing.T) {
 	}
 
 	if !filter.Match(testLine2) {
-		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine1, filter)
+		t.Errorf("testLine2 (%s) should not match filter (%s)", testLine2, filter)
+	}
+}
+
+func TestAndWorksBetweenFilterExpressions(t *testing.T) {
+	filter := NewColumnFilter("$1 == \"1\" AND $3 == \"teststring\"")
+	testLineBothMatches := &parser.LogLine{Columns: []string{"1", "", "teststring"}}
+	testLineFirstMatches := &parser.LogLine{Columns: []string{"1", "", "string"}}
+	testLineSecondMatches := &parser.LogLine{Columns: []string{"2", "", "teststring"}}
+	testLineNothingMatches := &parser.LogLine{Columns: []string{"2", "", "string"}}
+
+	if !filter.Match(testLineBothMatches) {
+		t.Errorf("testLineBothMatches (%s) should match filter (%s)", testLineBothMatches, filter)
+	}
+
+	if filter.Match(testLineFirstMatches) {
+		t.Errorf("testLineFirstMatches (%s) should not match filter (%s)", testLineFirstMatches, filter)
+	}
+
+	if filter.Match(testLineSecondMatches) {
+		t.Errorf("testLineSecondMatches (%s) should not match filter (%s)", testLineSecondMatches, filter)
+	}
+
+	if filter.Match(testLineNothingMatches) {
+		t.Errorf("testLineNothingMatches (%s) should not match filter (%s)", testLineNothingMatches, filter)
+	}
+}
+
+func TestOrWorksBetweenFilterExpressions(t *testing.T) {
+	filter := NewColumnFilter("$1 == \"1\" OR $3 == \"teststring\"")
+	testLineBothMatches := &parser.LogLine{Columns: []string{"1", "", "teststring"}}
+	testLineFirstMatches := &parser.LogLine{Columns: []string{"1", "", "string"}}
+	testLineSecondMatches := &parser.LogLine{Columns: []string{"2", "", "teststring"}}
+	testLineNothingMatches := &parser.LogLine{Columns: []string{"2", "", "string"}}
+
+	if !filter.Match(testLineBothMatches) {
+		t.Errorf("testLineBothMatches (%s) should match filter (%s)", testLineBothMatches, filter)
+	}
+
+	if !filter.Match(testLineFirstMatches) {
+		t.Errorf("testLineFirstMatches (%s) should not match filter (%s)", testLineFirstMatches, filter)
+	}
+
+	if !filter.Match(testLineSecondMatches) {
+		t.Errorf("testLineSecondMatches (%s) should not match filter (%s)", testLineSecondMatches, filter)
+	}
+
+	if filter.Match(testLineNothingMatches) {
+		t.Errorf("testLineNothingMatches (%s) should not match filter (%s)", testLineNothingMatches, filter)
+	}
+}
+
+func TestOperatorPrecedence(t *testing.T) {
+	filter := NewColumnFilter("$1 == \"1\" OR $2 == \"second\" AND $3 == \"teststring\"")
+	testLineMatches1 := &parser.LogLine{Columns: []string{"1", "no match", "no match"}}
+	testLineMatches2 := &parser.LogLine{Columns: []string{"no match", "second", "teststring"}}
+	testLineMatches3 := &parser.LogLine{Columns: []string{"1", "second", "teststring"}}
+	testLineNoMatch1 := &parser.LogLine{Columns: []string{"no match", "second", "no match"}}
+	testLineNoMatch2 := &parser.LogLine{Columns: []string{"no match", "no match", "second"}}
+
+	if !filter.Match(testLineMatches1) {
+		t.Errorf("testLineMatches1 (%s) should match filter (%s)", testLineMatches1, filter)
+	}
+
+	if !filter.Match(testLineMatches2) {
+		t.Errorf("testLineMatches2 (%s) should match filter (%s)", testLineMatches2, filter)
+	}
+
+	if !filter.Match(testLineMatches3) {
+		t.Errorf("testLineMatches3 (%s) should match filter (%s)", testLineMatches3, filter)
+	}
+
+	if filter.Match(testLineNoMatch1) {
+		t.Errorf("testLineNoMatch1 (%s) should not match filter (%s)", testLineNoMatch1, filter)
+	}
+
+	if filter.Match(testLineNoMatch2) {
+		t.Errorf("testLineNoMatch2 (%s) should not match filter (%s)", testLineNoMatch2, filter)
 	}
 }
